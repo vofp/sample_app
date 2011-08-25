ac83f5050b408e4b06ad1c3cb33038b610776e6f
Francis Vo <firefreakfrancis@gmail.com>
2011-08-25 13:49:18 -0700

    working feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   
   def feed
     # This is preliminary. See Chapter 12 for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(followed)
